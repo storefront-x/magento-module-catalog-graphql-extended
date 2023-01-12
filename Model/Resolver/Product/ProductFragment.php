217c55a8f98ce358eb74818e0da833a77b15bc95
simeunovicg <simeunovicg@magexo.cz>
2023-01-12 09:44:36 +0100

    PX-1430

--- Model/Resolver/Product/ProductFragment.php
+++ Model/Resolver/Product/ProductFragment.php
@@ -2,29 +2,14 @@
 
 namespace StorefrontX\CatalogGraphQlExtended\Model\Resolver\Product;
 
-use GraphQL\Language\AST\NodeKind;
-use Magento\Framework\GraphQl\Query\FieldTranslator;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
 /**
- * Class EntityUrlPlugin
+ * Class ProductFragment
  * @package  StorefrontX\CatalogGraphQlExtended\Plugin\Product
  */
 class ProductFragment extends \Magento\CatalogGraphQl\Model\Resolver\Product\ProductFieldsSelector
 {
-    /**
-     * @var FieldTranslator
-     */
-    private $fieldTranslator;
-
-    /**
-     * @param FieldTranslator $fieldTranslator
-     */
-    public function __construct(FieldTranslator $fieldTranslator)
-    {
-        parent::__construct($fieldTranslator);
-    }
-
 
     /**
      * Return field names for all requested product fields.
@@ -35,7 +20,6 @@ class ProductFragment extends \Magento\CatalogGraphQl\Model\Resolver\Product\Pro
      */
     public function getProductFieldsFromInfo(ResolveInfo $info, string $productNodeName = 'product') : array
     {
-        parent::getProductFieldsFromInfo($info, $productNodeName);
         return array_keys($info->getFieldSelection(0));
     }
 
